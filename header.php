1b9e19231454597915e58d41dabf813994fa2b2f
Dave Allen <davidallen401@btinternet.com>
2016-06-24 20:22:33 +0100

    Move nav to top

--- header.php
+++ header.php
@@ -25,6 +25,10 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'underdag' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'underdag' ); ?></button>
+		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+	</nav><!-- #site-navigation -->
 		<div class="site-branding">
 			<div class="logo">
 			<a href="<?php echo home_url(); ?>">
@@ -61,11 +65,7 @@
 			<?php
 			endif; ?>
 		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'underdag' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		
+			</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
