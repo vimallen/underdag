c19bb68148588b7a11fb0722670147b51aa9bdfb
Dave Allen <davidallen401@btinternet.com>
2016-06-24 22:32:50 +0100

    Trying 100% width

--- header.php
+++ header.php
@@ -20,7 +20,8 @@
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+
+<body <?php body_class(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'underdag' ); ?></a>
 
@@ -65,7 +66,7 @@
 			<?php
 			endif; ?>
 		</div><!-- .site-branding -->
-		
+
 			</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
